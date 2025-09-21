671c116c43382889b44336517b10a117be0222ae
Amit <amitmahich40@gmail.com>
2025-09-22 02:26:09 +0530

    Some changed applied

--- src/db.js
+++ src/db.js
@@ -3,8 +3,8 @@ import Dexie from "dexie";
 export const db = new Dexie("TalentFlowDB");
 
 db.version(1).stores({
-  jobs: "id, title, slug, status, order, tags, requiredSkills, preferredSkills, salary, location, workType, candidates",
-  candidates: "id, jobId, name, email, stage", // stage = Applied / Screening / Interview / Offer / Hired
-  assessments: "id, jobId, sections, createdAt, updatedAt",
-  candidateAnswers: "id, jobId, candidateEmail, answers, score, createdAt", // New table
+  jobs: "id, slug, status", // sirf searchable fields ko hi index karo
+  candidates: "id, jobId, email, stage",
+  assessments: "id, jobId, createdAt",
+  candidateAnswers: "id, jobId, candidateEmail, createdAt",
 });
